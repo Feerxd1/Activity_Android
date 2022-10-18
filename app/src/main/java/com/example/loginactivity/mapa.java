df63bf414063be1caa24899a39896b241551e522
Fxx Morgan <Avviouser77@gmail.com>
2022-10-18 20:43:41 -0300

    Actualizado Latitud Longitud & Altura

--- app/src/main/java/com/example/loginactivity/mapa.java
+++ app/src/main/java/com/example/loginactivity/mapa.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,38 +16,47 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
 
 import java.util.Locale;
 
+
 public class mapa extends AppCompatActivity {
 
     TextView mensaje1;
     TextView mensaje2;
+    TextView mensaje3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
-        mensaje1 = (TextView) findViewById(R.id.mensaje1);
-        mensaje2 = (TextView) findViewById(R.id.mensaje2);
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
         }
 
         // get my coordinates and show them in the textview
 
+        mensaje1 = (TextView) findViewById(R.id.mensaje1);
+        mensaje2 = (TextView) findViewById(R.id.mensaje2);
+        mensaje3 = (TextView) findViewById(R.id.mensaje3);
+
+        // get the location manager
+
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // define the location listener
+
         LocationListener locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                mensaje1.setText(""+ location.getLatitude());
-                mensaje2.setText(""+ location.getLongitude());
+                mensaje1.setText("" + location.getLatitude());
+                mensaje2.setText("" + location.getLongitude());
+                mensaje3.setText("" + location.getAltitude() + ", Precision: " + location.getAccuracy());
             }
 
             @Override
@@ -71,26 +81,50 @@ public class mapa extends AppCompatActivity {
 
             @Override
             public void onProviderDisabled(String provider) {
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(i);
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
             }
         };
 
-        // get the location updates
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 20, 0, locationListener);
+        // request location updates
 
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-        // open google maps with the location of the user when the button is pressed
-        Button button = (Button) findViewById(R.id.map);
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + mensaje1.getText().toString() + "," + mensaje2.getText().toString()));
-            startActivity(intent);
+        // get the last known location
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        // show the last known location
+
+        if (location != null) {
+            mensaje1.setText("" + location.getLatitude());
+            mensaje2.setText("" + location.getLongitude());
+            mensaje3.setText("Altitude: " + location.getAltitude() + ", Accuracy: " + location.getAccuracy());
+        }
+
+        // show the location in a map when the button is clicked
+
+        findViewById(R.id.map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    String uri = String.format(Locale.ENGLISH, "geo:%f,%f", location.getLatitude(), location.getLongitude());
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    startActivity(intent);
+                }
+            }
         });
 
+
+
+
+
+
+
+
+
+
     }
-
-
-
 }
-
 
